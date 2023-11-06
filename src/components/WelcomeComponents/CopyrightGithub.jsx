cd7e96663ec1218fadb4ecd67a0b895b9c678176
Admin <admin@Admins-MacBook-Air.local>
2023-11-06 14:02:01 +0000

    completed copyright and github section

---
+++ src/components/WelcomeComponents/CopyrightGithub.jsx
@@ -0,0 +1,18 @@
+export default function CopyrightGithub () {
+    return (
+        <div>
+            <h4>The Team Github</h4>
+                <a href="https://github.com/tomalcock" target='blank'>Sting-arrays</a>
+            <h5>Contributors</h5>
+          <ul id='authors-list'>
+            <li><a href="https://github.com/tomalcock" target='blank'>Tom Lacy-ALcock</a></li>
+            <li><a href="https://github.com/Isaac-Madden" target='blank'>Isaac Madden</a></li>
+            <li><a href="https://github.com/JordanEckford" target='blank'>Jordan Eckford</a></li>
+            <li><a href="https://github.com/muxappo" target='blank'>Max Field</a></li>
+            <li><a href="https://github.com/LukeRiches" target='blank'>Luke Riches</a></li>
+          </ul>
+
+          <p>© 2023 sting-arrays</p>
+        </div>
+    )
+}
---
+++ src/components/WelcomeComponents/Instructions.jsx
@@ -0,0 +1,3 @@
+export default function Instructions() {
+    return <p>Welcome to Pixel-Plunge, the game where you can explore the depths of the ocean and collecting. Beware: Some of these fish bite! Get ready to take the plunge...</p>
+}
