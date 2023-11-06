13bd10475d0907cf6cb676c747225e3c2d1a4205
LukeRiches <Lukejr2603@gmail.com>
2023-11-06 10:50:01 +0000

    Added background and boat

--- src/PhaserApp/Scenes/MainGame.js
+++ src/PhaserApp/Scenes/MainGame.js
@@ -1,14 +1,26 @@
 import Phaser from "phaser";
+import background from "../../assets/Background/background.png";
+import boat from "../../assets/Scenary/boat.png";
+
+let fixed;
 
 export class MainGame extends Phaser.Scene {
- constructor() {
-  super("maingame");
- }
+  constructor() {
+    super("maingame");
+  }
 
- init() {}
- preload() {}
- create() {
-  this.add.text(400, 300, "MainGame Scene");
- }
- update() {}
+  init() {}
+  preload() {
+    this.load.image("background", background);
+    this.load.image("boat", boat);
+  }
+  create() {
+    this.add.image(400, 1000, "background");
+
+    fixed = this.physics.add.staticGroup();
+
+    fixed.create(40, 75, "boat");
+  }
+
+  update() {}
 }
