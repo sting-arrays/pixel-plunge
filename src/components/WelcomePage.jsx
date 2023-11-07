eae7f085005839aa7a176198a6e8c99680db6691
JordanEckford <jordan.eckford@gmail.com>
2023-11-07 13:44:10 +0000

    fixed previous merge problems

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,19 +1,18 @@
-
-import Title from './Title';
-import Instructions from './Instructions';
-import StartGame from './StartGame';
-import addMaxUser from '../firebase'
-import UserDetailsTest from './UserDetailsTest';
-import CopyrightGithub from './WelcomeComponents/CopyrightGithub';
+import Title from "./WelcomeComponents/Title";
+import Instructions from "./WelcomeComponents/Instructions";
+import StartGame from "./WelcomeComponents/StartGame";
+import addMaxUser from "../firebase";
+import UserDetailsTest from "./UserDetailsTest";
+import CopyrightGithub from "./WelcomeComponents/CopyrightGithub";
 
 export default function WelcomePage() {
-    return (
-        <div>
-            <Title />
-            <Instructions />
-            <StartGame />
-            <UserDetailsTest />
-            <CopyrightGithub />
-        </div>
-    )
+ return (
+  <div>
+   <Title />
+   <Instructions />
+   <StartGame />
+   <UserDetailsTest />
+   <CopyrightGithub />
+  </div>
+ );
 }
