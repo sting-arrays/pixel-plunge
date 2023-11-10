bff494669cb2f39269b3e2356d26b2156c170d8e
Admin <admin@Admins-MacBook-Air.local>
2023-11-10 13:41:15 +0000

    made changes to load different images

--- src/components/StartGameComponents/Fishidex.jsx
+++ src/components/StartGameComponents/Fishidex.jsx
@@ -1,11 +1,13 @@
 import GamePage from "../GamePage";
 import { Link } from "react-router-dom";
 import { getAllFish } from "../../firebase";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
+import { UserNameContext } from "../../contexts/UsernameContext";
 
 export default function Fishidex() {
 
     const [ allFish, setAllFish ] = useState([])
+    const { currentUser, setCurentUser } = useContext(UserNameContext);
 
     useEffect (() => {
         getAllFish()
@@ -24,13 +26,23 @@ export default function Fishidex() {
             <p>This is fishidex!</p>
             <ul>
             {allFish.map((fish) => {
-                return (
+                if(currentUser === 'Guest') {
+                    return (
+                        <div key={fish.name}>
+                            <li><img src={fish.image} alt='fish swimming in the sea and having a great time'/></li>
+                            <li>{fish.name}</li>
+                            <li>{fish.facts}</li>
+                        </div>
+                    )
+                } return (
                     <div key={fish.name}>
-                        <li><img src={fish.image} alt='fish swimming in the sea and having a great time'/></li>
-                        <li>{fish.name}</li>
-                        <li>{fish.facts}</li>
+                            <li><img src={fish.grey_image} alt='fish swimming in the sea and having a great time'/></li>
+                            <li>{fish.name}</li>
+                            <li>{fish.facts}</li>
                     </div>
-                )}
+                )
+                }
+                
             )}
             </ul>
             <Link className='button' to='/game' element={<GamePage />}> Back to Game </Link>
