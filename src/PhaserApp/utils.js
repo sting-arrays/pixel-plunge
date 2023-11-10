e43d1dc6fc00ce6f16e14752c96f61fa98fa552e
Max Field <maxfield98@outlook.com>
2023-11-10 00:00:30 +0000

    fixed vertical swimming animation

--- src/PhaserApp/utils.js
+++ src/PhaserApp/utils.js
@@ -1,35 +1,47 @@
-export function createUniqueFish(amount, minY, maxY, fishGroup, image, yUpperLimit, yLowerLimit) {
- for (let i = 0; i < amount; i++) {
-  let y = Phaser.Math.Between(minY, maxY);
-  let x = Phaser.Math.Between(0, 600);
-  let fish = fishGroup.create(x, y, image).setScale(0.2);
-  fish.setCollideWorldBounds(false);
-  fish.setSize(70, 50, true);
+export function createUniqueFish(
+  amount,
+  minY,
+  maxY,
+  fishGroup,
+  image,
+  yUpperLimit,
+  yLowerLimit
+) {
+  for (let i = 0; i < amount; i++) {
+    let y = Phaser.Math.Between(minY, maxY);
+    let x = Phaser.Math.Between(0, 600);
+    let fish = fishGroup.create(x, y, image).setScale(0.2);
+    fish.setCollideWorldBounds(false);
+    fish.setSize(70, 50, true);
 
-  setInterval(() => {
-   if (fish.body !== undefined) {
-    if (fish.y < yUpperLimit) {
-     fish.setVelocityY(30);
-    } else if (fish.y > yLowerLimit) {
-     fish.setVelocityY(-30);
-    } else if (fish.x < -100) {
-     fish.setVelocityX(Phaser.Math.Between(50, 200));
-    } else if (fish.x > 900) {
-     fish.setVelocityX(Phaser.Math.Between(-50, -150));
-    } else {
-     fish.setVelocityX(Phaser.Math.Between(-200, 200));
-     fish.setVelocityY(Phaser.Math.Between(-30, 30));
-    }
+    setInterval(() => {
+      if (fish.body !== undefined) {
+        if (fish.y < yUpperLimit) {
+          fish.setVelocityY(30);
+        } else if (fish.y > yLowerLimit) {
+          fish.setVelocityY(-30);
+        } else if (fish.x < -100) {
+          fish.setVelocityX(Phaser.Math.Between(50, 200));
+        } else if (fish.x > 900) {
+          fish.setVelocityX(Phaser.Math.Between(-50, -150));
+        } else {
+          fish.setVelocityX(Phaser.Math.Between(-200, 200));
+          fish.setVelocityY(Phaser.Math.Between(-30, 30));
+        }
 
-    if (fish.body.velocity.x < 0) {
-     fish.flipX = true;
-    } else {
-     fish.flipX = false;
-    }
-   }
-  }, Phaser.Math.Between(1000, 3000));
+        if (fish.body.velocity.x < 0) {
+          fish.flipX = true;
+        } else {
+          fish.flipX = false;
+        }
+      }
+    }, Phaser.Math.Between(1000, 3000));
 
-  fish.setVelocity(Phaser.Math.Between(-300, 300), 0);
-  fish.body.setAllowGravity(false);
- }
+    fish.setVelocity(Phaser.Math.Between(-300, 300), 0);
+    fish.body.setAllowGravity(false);
+  }
+}
+
+export function between(item, val1, val2) {
+  return item >= val1 && item <= val2;
 }
--- src/components/GamePage.jsx
+++ src/components/GamePage.jsx
@@ -6,17 +6,29 @@ import Upgrades from "./StartGameComponents/Upgrades";
 import WelcomePage from "./WelcomePage";
 
 export default function GamePage() {
-    const userName = "Jordan_Ekford"
+  const userName = "Jordan_Ekford";
 
-    return(
-        <>
-            <AppBridge width={800} height={600} userName={userName} />
-            <div className='nav-bar'>
-                <Link className='button' to='/fishidex' element={ <Fishidex /> }> Fishidex </Link>
-                <Link className='button' to='/leaderboard' element={ <LeaderBoard /> }> Leader Board </Link>
-                <Link className='button' to='/upgrades' element={ <Upgrades /> }> Upgrades </Link>
-                <Link className='button' to='/' element={<WelcomePage />}> Home </Link>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <AppBridge width={800} height={600} userName={userName} />
+      <div className="nav-bar">
+        <Link className="button" to="/fishidex" element={<Fishidex />}>
+          {" "}
+          Fishidex{" "}
+        </Link>
+        <Link className="button" to="/leaderboard" element={<LeaderBoard />}>
+          {" "}
+          Leader Board{" "}
+        </Link>
+        <Link className="button" to="/upgrades" element={<Upgrades />}>
+          {" "}
+          Upgrades{" "}
+        </Link>
+        <Link className="button" to="/" element={<WelcomePage />}>
+          {" "}
+          Home{" "}
+        </Link>
+      </div>
+    </>
+  );
 }
