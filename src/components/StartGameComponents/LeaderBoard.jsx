ea0d1f5386c98de635479a6c98f8d179f21685ca
JordanEckford <jordan.eckford@gmail.com>
2023-11-15 13:22:05 +0000

    fixed layout problems on leaderboard component

--- src/components/StartGameComponents/LeaderBoard.jsx
+++ src/components/StartGameComponents/LeaderBoard.jsx
@@ -4,35 +4,42 @@ import GamePage from "../GamePage";
 import { Link } from "react-router-dom";
 
 export default function LeaderBoard() {
-    let counter = 1;
-    const [users, setUsers] = useState([])
-    const [loading, setLoading] = useState(true)
+ let counter = 1;
+ const [users, setUsers] = useState([]);
+ const [loading, setLoading] = useState(true);
 
-    useEffect( () => {
-        getAllUsers().then( result => {
-            setUsers(result.sort( (a,b) => b.Fish_Count - a.Fish_Count).slice(0,10)); 
-            setLoading(false); 
-        })
-    }, [])
+ useEffect(() => {
+  getAllUsers().then((result) => {
+   setUsers(result.sort((a, b) => b.Fish_Count - a.Fish_Count).slice(0, 10));
+   setLoading(false);
+  });
+ }, []);
 
-    if(loading) return <p>is loading...</p>
- 
-    return(
-        <div className='absolute'>
-            <img className='relative' alt="Pixel Plunge" src="/scroll-leaderboard-2.png" />
-            <p className='relative text-[35px] mt-[-90%]'>Leaderboard</p>
-            <ul className="relative text-[35px] mt-[10%]">
-            {users.map( user => { 
-                return(
-                    <li key={user.userName}>
-                        <p className='text-[20px]'>{counter++ + ' - ' + user.userName}</p>
-                        <div></div>
-                        <p className='text-[14px] mb-2'>Fish Caught: {user.Fish_Count}</p>
-                    </li>
-                )
-            })}
-            </ul>
-            <Link className='button relative  mt-[4%]' to='/game' element={<GamePage />}> Back to Game </Link>
-        </div>  
-    )
+ if (loading) return <p>is loading...</p>;
+
+ return (
+  <div className="m-auto">
+   {/* <img className="" alt="Pixel Plunge" src="/scroll-leaderboard-2.png" /> */}
+   <div className="bg-[url('./public/scroll-leaderboard-2.png')] bg-contain bg-no-repeat bg-center min-h-[820px] min-w-[870px] overflow-auto m-auto pb-60">
+    <p className=" text-[35px] pt-28 ">Leaderboard</p>
+    <div className="min-h-[500px]">
+     <ul className=" text-[32px] pt-24 min-h-[200px] ">
+      {users.map((user) => {
+       return (
+        <li key={user.userName}>
+         <p className="text-[20px]">{counter++ + " - " + user.userName}</p>
+         <div></div>
+         <p className="text-[12px] mb-1">Fish Caught: {user.Fish_Count}</p>
+        </li>
+       );
+      })}
+     </ul>
+    </div>
+   </div>
+   <Link className="button relative  mt-[4%]" to="/game" element={<GamePage />}>
+    {" "}
+    Back to Game{" "}
+   </Link>
+  </div>
+ );
 }
